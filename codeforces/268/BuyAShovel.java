d04f2777f8290fb5f079cdd1842872979b2cb169
Shubham Kumar <shubharkumar9@gmail.com>
2026-09-10 22:00:58 +0530

    53th probleam solved

---
+++ codeforces/268/BuyAShovel.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class BuyAShovel {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int k = scanner.nextInt();
+        int r = scanner.nextInt();
+        
+        for (int i = 1; i <= 10; i++) {
+            int totalCost = k * i;
+            if (totalCost % 10 == 0 || totalCost % 10 == r) {
+                System.out.println(i);
+                break;
+            }
+        }
+    }
+}
