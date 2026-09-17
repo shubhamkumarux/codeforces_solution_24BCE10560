ad0c7d2ea5bc0a9213d59c35c59e78335e01f481
Shubham Kumar <shubharkumar9@gmail.com>
2026-09-17 21:51:29 +0530

    59th probleam solvead

---
+++ codeforces/268/codeforces/1352/codeforces/Codeforces/Again Twenty Five.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Again Twenty Five {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.ext);
+        // n is read but not needed because the answer is always 25 for n >= 2
+        long n = sc.nextLong();
+        System.out.println(25);
+    }
+}
